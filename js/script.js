296ce2c041cc5b8d85569b5201d70eecd390c018
Pasha <pberkal@bk.ru>
2022-09-14 21:24:27 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict"
+const a = 'popa';
+console.log(`https://someurl.com/${a}/4`);
+alert(`Привет, ${a}`);
+
+
+
+const cards = [
+    {
+        id: 1,
+        title: 'Rolex',
+        description: 'Элитные часы',
+        price: '1 mln',
+    },
+    {
+        id: 2,
+        title: 'GUCCI',
+        description: 'Красивые часы',
+        price: '100 k',
+    },
+    {
+        id: 3,
+        title: 'Cassio',
+        description: 'Хорошие часы по приемлемой цене',
+        price: '15 k',
+    },
+];
